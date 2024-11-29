9b6d03bfe7549587853f03dd93f4e13089fa8ca7
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2024-11-29 18:23:30 -0500

    Initial network functionality added

--- Assets/Scripts/NetworkManagerUI.cs
+++ Assets/Scripts/NetworkManagerUI.cs
@@ -1,28 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class NetworkManagerUI : MonoBehaviour
 {
-    [SerializeField] private Button serverBtn;
-    [SerializeField] private Button hostBtn;
-    [SerializeField] private Button clientBtn;
+    //[SerializeField] private Button serverBtn;
+    [SerializeField] private GameObject joinMenu;
+    [SerializeField] private TMP_Text joinCode;
+    [SerializeField] private TMP_InputField joinInputField;
+    [SerializeField] private TestRelay testRelay;
 
-    private void Awake()
+    private string joinCodeStr;
+
+    protected void Awake()
     {
-        serverBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartServer();
-        });
-        hostBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartHost();
-        });
-        clientBtn.onClick.AddListener(() =>
-        {
-            NetworkManager.Singleton.StartClient();
-        });
+        //serverBtn.onClick.AddListener(() =>
+        //{
+        //    NetworkManager.Singleton.StartServer();
+        //});
+        //hostBtn.onClick.AddListener(() =>
+        //{
+        //    NetworkManager.Singleton.StartHost();
+        //});
+
+        joinCode.text = string.Empty;
+        DisableJoinIF();
+    }
+
+    public void EnableJoinIF()
+    {
+        joinMenu.SetActive(true);
+    }
+
+    public void DisableJoinIF()
+    {
+        joinMenu.SetActive(false);
+        joinCodeStr = string.Empty;
+        joinInputField.text = string.Empty;
+    }
+
+    public void SetJoinCode(string joinCode)
+    {
+        joinCodeStr = joinCode;
+        this.joinCode.text = "Code: " + joinCodeStr;
+    }
+
+    public void JoinLobby()
+    {
+        testRelay.JoinRelay(joinCodeStr);
+    }
+
+    public void UpdateJoinCode(string joinCodeStr)
+    {
+        this.joinCodeStr = joinCodeStr;
     }
 }
--- Assets/Scripts/PlayerNetwork.cs
+++ Assets/Scripts/PlayerNetwork.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -9,28 +10,82 @@ public class PlayerNetwork : NetworkBehaviour
     [SerializeField] private float moveSpeed = 3f;
     private PlayerInputActions inputActions;
 
+    [SerializeField] private Transform spawnedObjectPrefab;
+    private Transform spawnedObjectTransform;
+
+    private NetworkVariable<prueba> randomNumber = new NetworkVariable<prueba>(
+        new prueba
+        {
+            _int = 1,
+            _bool = true
+        }
+        , NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct prueba : INetworkSerializable
+    {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
     private Vector2 moveInput;
 
-    private void Awake()
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        randomNumber.OnValueChanged += (prueba previousValue, prueba newValue) =>
+        {
+            print(OwnerClientId + ": " + newValue._int + "; " + newValue._bool + "; " + newValue.message);
+        };
+    }
+
+    protected void Awake()
     {
         inputActions = new PlayerInputActions();
     }
 
-    private void Update()
+    protected void Update()
     {
+        //Check if client is owner. If it's not, it can't move the player
         if (!IsOwner) return;
         Vector3 movement = new Vector3(moveInput.x, moveInput.y, 0) * moveSpeed * Time.deltaTime;
         transform.position += movement;
+        //test
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            spawnedObjectTransform = Instantiate(spawnedObjectPrefab);
+            spawnedObjectTransform.GetComponent<NetworkObject>().Spawn(true);
+            //TestClientRpc();
+            //TestServerRpc();
+            //randomNumber.Value = new prueba
+            //{
+            //    _int = 10,
+            //    _bool = false,
+            //    message = "Gaaaaa"
+            //};
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Destroy(spawnedObjectTransform.gameObject);
+        }
     }
 
-    private void OnEnable()
+    protected void OnEnable()
     {
         inputActions.Enable();
         inputActions.PlayerControls.move.performed += OnMove;
         inputActions.PlayerControls.move.canceled += OnMove;
     }
 
-    private void OnDisable()
+    protected void OnDisable()
     {
         inputActions.PlayerControls.move.performed -= OnMove;
         inputActions.PlayerControls.move.canceled -= OnMove;
@@ -41,5 +96,18 @@ public class PlayerNetwork : NetworkBehaviour
     {
         if (!IsOwner) return;
         moveInput = context.ReadValue<Vector2>();
+        print(OwnerClientId);
+    }
+
+    [ServerRpc]
+    private void TestServerRpc()
+    {
+        print("TestServerRpc " + OwnerClientId);
+    }
+
+    [ClientRpc]
+    private void TestClientRpc()
+    {
+        print("TestClientRpc");
     }
 }
