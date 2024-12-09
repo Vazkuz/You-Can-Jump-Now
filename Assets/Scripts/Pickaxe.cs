0d28011127e178bdc51a07e2e341ce354e74f954
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2024-12-08 22:47:03 -0500

    Pickaxe owner changes when grabbed

--- Assets/Scripts/Pickaxe.cs
+++ Assets/Scripts/Pickaxe.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,12 +7,14 @@ using UnityEngine;
 public class Pickaxe : NetworkBehaviour
 {
     private Rigidbody2D rb;
-    private Collider2D collider2D;
+    private Collider2D _collider2D;
+    private NetworkObject networkObject;
 
     protected void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<Collider2D>();
+        _collider2D = GetComponent<Collider2D>();
+        networkObject = GetComponent<NetworkObject>();
     }
     // Start is called before the first frame update
     public override void OnNetworkObjectParentChanged(NetworkObject parentNetworkObject)
@@ -22,9 +25,20 @@ public class Pickaxe : NetworkBehaviour
         if (parentNetworkObject.GetComponent<PlayerNetwork>() == null) return;
         
         rb.isKinematic = true;
-        collider2D.enabled = false;
+        _collider2D.enabled = false;
         transform.localPosition = new Vector3(0, 0 , 0);
-
+        if (!IsServer) RequestChangeOwnershipRpc(parentNetworkObject.OwnerClientId);
+        else ChangePickaxeOwnership(parentNetworkObject.OwnerClientId);
     }
 
+    [Rpc(SendTo.Server)]
+    private void RequestChangeOwnershipRpc(ulong newClientId)
+    {
+        ChangePickaxeOwnership(newClientId);
+    }
+
+    private void ChangePickaxeOwnership(ulong newClientId)
+    {
+        networkObject.ChangeOwnership(newClientId);
+    }
 }
