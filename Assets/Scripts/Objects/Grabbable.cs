0804b5d7d4940a999d92baa279031b3e6459fdf8
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2024-12-21 15:24:57 -0500

    Transferred all pickaxe info to grabbable

--- Assets/Scripts/Objects/Grabbable.cs
+++ Assets/Scripts/Objects/Grabbable.cs
@@ -5,15 +5,58 @@ using UnityEngine;
 
 public class Grabbable : NetworkBehaviour
 {
+    protected Rigidbody2D rb;
+    [SerializeField] protected Collider2D triggerCollider;
+    private NetworkObject networkObject;
+    private bool justSpawned = true;
     // Start is called before the first frame update
-    void Start()
+    protected virtual void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        networkObject = GetComponent<NetworkObject>();
+    }
+    // Start is called before the first frame update
+    public override void OnNetworkObjectParentChanged(NetworkObject parentNetworkObject)
+    {
+        base.OnNetworkObjectParentChanged(parentNetworkObject);
+
+        if (justSpawned)
+        {
+            justSpawned = false;
+            return;
+        }
+        else if (transform.parent == null)
+        {
+            rb.isKinematic = false;
+            triggerCollider.enabled = true;
+
+            // Giving the pickaxe back to the server.
+            if (!IsServer) RequestChangeOwnershipRpc(NetworkManager.ServerClientId);
+            else ChangePickaxeOwnership(NetworkManager.ServerClientId);
+            return;
+        }
+
+        if (parentNetworkObject == null) return;
+        if (parentNetworkObject.GetComponent<PlayerNetwork>() == null) return;
+
+        rb.isKinematic = true;
+        triggerCollider.enabled = false;
+
+        transform.localPosition = parentNetworkObject.GetComponent<PlayerNetwork>().Hand.localPosition;
+        if (!IsServer) RequestChangeOwnershipRpc(parentNetworkObject.OwnerClientId);
+        else ChangePickaxeOwnership(parentNetworkObject.OwnerClientId);
     }
 
-    // Update is called once per frame
-    void Update()
+    [Rpc(SendTo.Server)]
+    private void RequestChangeOwnershipRpc(ulong newClientId)
     {
-        
+        ChangePickaxeOwnership(newClientId);
+    }
+
+    private void ChangePickaxeOwnership(ulong newClientId)
+    {
+        if (networkObject.OwnerClientId == newClientId) return;
+
+        networkObject.ChangeOwnership(newClientId);
     }
 }
