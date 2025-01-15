d55dcd22303b37910faeff4d95a17e86aa104359
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2025-01-15 16:42:57 -0500

    Added summary to PlateInteractable

--- Assets/Scripts/Player/PlateInteractable.cs
+++ Assets/Scripts/Player/PlateInteractable.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
+/// <summary>
+/// This class is for the objects that will interact with pressure plates. For example: Players, Gold and stuff that can put pressure on plates.
+/// </summary>
 public class PlateInteractable : NetworkBehaviour
 {
     //public float weight { get { return _weight; } private set { _weight = value; } }
