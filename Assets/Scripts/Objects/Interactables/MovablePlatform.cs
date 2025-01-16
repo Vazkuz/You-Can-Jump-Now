b5c35bd6fbcf6662c04577d714357a2c70c57f93
Daniel Madalengoitia <d.madalengoitia@pucp.edu.pe>
2025-01-16 16:52:20 -0500

    Creating moving platform

---
+++ Assets/Scripts/Objects/Interactables/MovablePlatform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePlatform : TriggerTarget
+{
+    [SerializeField] private Transform inactivePos;
+    [SerializeField] private Transform activePos;
+    [SerializeField] private float speed = 5f;
+    public override void Activate()
+    {
+        base.Activate();
+    }
+
+    public override void Deactivate()
+    {
+        base.Deactivate();
+    }
+
+    protected void Update()
+    {
+        if (isActive.Value && transform.position == activePos.position) return;
+        if (!isActive.Value && transform.position == inactivePos.position) return;
+
+        if (isActive.Value)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, activePos.position, speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, inactivePos.position, speed * Time.deltaTime);
+        }
+    }
+
